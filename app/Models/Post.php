82c1224d3d47271ba7791263bde0f25417314487
manuelnones <manuelnones99@yahoo.com>
2023-05-24 16:43:50 +0200

    correct Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,6 @@ class Post extends Model
     protected $fillable = ['title', 'content', 'slug'];
 
     public function type() {
-        $this->belongsTo(Type::class);
+       return $this->belongsTo(Type::class);
     }
 }
