0f93c4891e8998defceb35b821b793ac0d62bca6
manuelnones <manuelnones99@yahoo.com>
2023-05-24 15:59:30 +0200

    add migrations types_table

--- database/migrations/2023_05_24_081736_create_types_table.php
+++ database/migrations/2023_05_24_081736_create_types_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 100)->unique();
+            $table->text('description')->nullable();
+            $table->string('slug', 100);
+
             $table->timestamps();
         });
     }
